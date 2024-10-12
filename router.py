273dd3ad31f31c3bda3a557a4a3d96a8f036dcf9
forizn <forizn00@gmail.com>
2024-10-12 21:27:07 +0300

    initial commit

---
+++ router.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from respository import TaskRepository
+from schemas import STaskAdd, STask, STaskId
+
+router = APIRouter(
+    prefix='/tasks',
+    tags=['Таски'],
+)
+
+
+@router.post('')
+async def add_tasks(
+        task: Annotated[STaskAdd, Depends()],
+) -> STaskId:
+    task_id = await TaskRepository.add_one(task)
+    return {'ok': True, 'task_id': task_id}
+
+
+@router.get('')
+async def get_tasks() -> list[STask]:
+    tasks = await TaskRepository.find_all()
+    return tasks
